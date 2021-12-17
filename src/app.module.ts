0cd1ba43f3fcb593a1e57b5af27fc8df1034c2f1
ted <sdgwon@lunasoft.co.kr>
2021-12-17 15:12:43 +0900

    검색 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScrapingModule } from '@scraping/scraping.module';
+import { SearchModule } from './modules/search/search.module';
 
 @Module({
-  imports: [ScrapingModule],
+  imports: [ScrapingModule, SearchModule],
   controllers: [AppController],
   providers: [
     AppService
---
+++ src/modules/search/search.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { FileService } from '../file/file.service';
+import { SearchService } from './search.service';
+
+@Controller('search')
+export class SearchController {
+
+  constructor(
+    private readonly searchService:SearchService, 
+    private readonly fileService:FileService
+  ){}
+  
+  @Get('inven/rank')
+  async getInvenRank()
+  {
+    return await this.fileService.readFile('scraping/inven.rank');
+  }
+}
